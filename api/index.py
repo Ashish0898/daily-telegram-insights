36d6b347fb7bd7e67c5b57be6add8084e7bb0382
Ashish Gupta <ashishg756@gmail.com>
2026-06-06 19:16:29 +0000

    Escape HTML special characters in help message for Telegram commands

--- api/index.py
+++ api/index.py
@@ -68,8 +68,8 @@ def build_help_message() -> str:
     return (
         "Hello! 🤖\n\n"
         "Use /fact to receive a new random fact.\n"
-        "Use /news or /news <query> to get Talivy news.\n"
-        "Use /search <query> to search Talivy for a custom topic.\n"
+        "Use /news or /news &lt;query&gt; to get Talivy news.\n"
+        "Use /search &lt;query&gt; to search Talivy for a custom topic.\n"
         "Use /help to show this message again."
     )
 
